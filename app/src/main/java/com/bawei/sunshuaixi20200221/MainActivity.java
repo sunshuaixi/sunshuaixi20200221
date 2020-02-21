0782c4afc87532d1c88882fb0a806fce66d48b50
孙帅喜 <2410370911@qq.com>
2020-02-21 13:36:42 +0800

    添加注释

--- app/src/main/java/com/bawei/sunshuaixi20200221/MainActivity.java
+++ app/src/main/java/com/bawei/sunshuaixi20200221/MainActivity.java
@@ -3,11 +3,15 @@ package com.bawei.sunshuaixi20200221;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 类注释
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
